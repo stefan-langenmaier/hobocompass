184d81b6b9739dcf23aa0509109a6850fdcad335
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-11-18 19:14:39 +0100

    add baseline test

---
+++ assets/javascripts/compass.test.js
@@ -0,0 +1,48 @@
+const Compass = require('./compass.js');
+
+test('fresh compass has empty summary', () => {
+  const compass = new Compass();
+  expect(compass.summary).toStrictEqual({"bearing": null, "orientation": null, "quality": null, "speed": null});
+});
+
+test('going north with the phone in north orientation', () => {
+  const compass = new Compass();
+  const fixture = require('./fixtures/trace-fp3-20201028-001.json');
+  compass.buffer = fixture;
+  compass.generateAverages();
+  compass.generateSummary();
+  // walking direction was 4 degress north
+  expect(compass.summary.bearing).toBeGreaterThan(4-1);
+  expect(compass.summary.bearing).toBeLessThan(4+1);
+  // carrying direction was about 4 degress north
+  expect(compass.summary.orientation).toBeGreaterThan(94-1);
+  expect(compass.summary.orientation).toBeLessThan(94+1);
+});
+
+test('going north with the phone in west orientation', () => {
+  const compass = new Compass();
+  const fixture = require('./fixtures/trace-fp3-20201118-001.json');
+  compass.buffer = fixture;
+  compass.generateAverages();
+  compass.generateSummary();
+  // walking direction was 4 degress north
+  expect(compass.summary.bearing).toBeGreaterThan(4-1);
+  expect(compass.summary.bearing).toBeLessThan(4+1);
+  // carrying direction was about 4 degress north
+  expect(compass.summary.orientation).toBeGreaterThan(75-1);
+  expect(compass.summary.orientation).toBeLessThan(75+1);
+});
+
+test('going east with the phone in east orientation', () => {
+  const compass = new Compass();
+  const fixture = require('./fixtures/trace-fp3-20201118-002.json');
+  compass.buffer = fixture;
+  compass.generateAverages();
+  compass.generateSummary();
+  // walking direction was 4 degress north
+  expect(compass.summary.bearing).toBeGreaterThan(96-1);
+  expect(compass.summary.bearing).toBeLessThan(96+1);
+  // carrying direction was about 4 degress north
+  expect(compass.summary.orientation).toBeGreaterThan(94-1);
+  expect(compass.summary.orientation).toBeLessThan(94+1);
+});
